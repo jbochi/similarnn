469f7ca7917c5a5d2fd41680371b413f3ce886ce
Juarez Bochi <jbochi@gmail.com>
2016-10-20 15:38:02 -0200

    Add private method for creating model storage

--- similarnn/storage/__init__.py
+++ similarnn/storage/__init__.py
@@ -8,5 +8,8 @@ class Storage(object):
     def get_model_db(self, model):
         if model.name in self.storage:
             return self.storage[model.name]
-        self.storage[model.name] = NearestNeighbours(model.num_topics)
+        self.storage[model.name] = self._create_model_db(model)
         return self.storage[model.name]
+
+    def _create_model_db(self, model):
+        return NearestNeighbours(model.num_topics)
