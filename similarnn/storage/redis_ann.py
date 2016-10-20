7a198f99d7f50256b695e04939e4d117f3689295
Juarez Bochi <jbochi@gmail.com>
2016-10-20 17:19:42 -0200

    Remove items and sync with redis

--- similarnn/storage/redis_ann.py
+++ similarnn/storage/redis_ann.py
@@ -8,20 +8,25 @@ class RedisNearestNeighbours(NearestNeighbours):
         super().__init__(n_factors, **kwargs)
         self.redis_conn = redis_conn
         self.namespace = namespace
-        self._load_items_from_redis()
+        self.sync()
 
     def add_item(self, key, vector):
         self._set_key_on_redis(key, vector)
         super().add_item(key, vector)
 
+    def remove_item(self, key):
+        self._del_key_on_redis(key)
+        super().remove_item(key)
+
     def is_synced(self):
         local, remote = self._local_keys(), dict(self._keys_from_redis())
         return local == remote
 
-    def _load_items_from_redis(self):
+    def sync(self):
+        self.clean()
         for key, vector in self._keys_from_redis():
-            super()._add_new_vector(key, vector)
-        super()._rebuild_index()
+            self._add_new_vector(key, vector)
+        self._rebuild_index()
 
     def _local_keys(self):
         return {key: self.vectors[i] for key, i in self.id_from_key.items()}
@@ -30,6 +35,9 @@ class RedisNearestNeighbours(NearestNeighbours):
         self.redis_conn.hset(self.namespace,
                              key.encode('utf-8'),
                              pickle.dumps(vector))
+
+    def _del_key_on_redis(self, key):
+        self.redis_conn.hdel(self.namespace, key)
 
     def _keys_from_redis(self):
         for key, vector in self.redis_conn.hgetall(self.namespace).items():
--- tests/storage/test_redis_ann.py
+++ tests/storage/test_redis_ann.py
@@ -31,6 +31,24 @@ def test_items_are_persisted(redis_conn):
     assert 3 == space.n_items
 
 
+def test_items_are_removed(redis_conn):
+    space = RedisNearestNeighbours(n_factors=2,
+                                   redis_conn=redis_conn,
+                                   namespace=NAMESPACE)
+    assert 0 == space.n_items
+
+    space.add_item("doc1", [0, 1])
+    space.add_item("doc2", [0, 2])
+    space.remove_item("doc1")
+    assert 1 == space.n_items
+
+    space2 = RedisNearestNeighbours(n_factors=2,
+                                    redis_conn=redis_conn,
+                                    namespace=NAMESPACE)
+    assert 1 == space2.n_items
+    assert [0, 2] == space2.item_vector("doc2")
+
+
 def test_rnn_is_synced_different_keys(redis_conn):
     space1 = RedisNearestNeighbours(n_factors=2,
                                     redis_conn=redis_conn,
@@ -62,3 +80,22 @@ def test_rnn_is_synced_different_values(redis_conn):
 
     assert not space1.is_synced()
     assert space2.is_synced()
+
+
+def test_sync(redis_conn):
+    space1 = RedisNearestNeighbours(n_factors=2,
+                                    redis_conn=redis_conn,
+                                    namespace=NAMESPACE)
+    space1.add_item("doc1", [0, 1])
+
+    space2 = RedisNearestNeighbours(n_factors=2,
+                                    redis_conn=redis_conn,
+                                    namespace=NAMESPACE)
+    space2.add_item("doc2", [1, 1])
+    space2.remove_item("doc1")
+
+    assert not space1.is_synced()
+    space1.sync()
+    assert space1.is_synced()
+    assert 1 == space1.n_items
+    assert [1, 1] == space1.item_vector("doc2")
